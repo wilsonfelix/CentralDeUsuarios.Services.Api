38365546010a4015292722dcc296b4b281dd5661
wandrade23 <wilson.felix.jr@gmail.com>
2023-05-11 03:56:34 -0300

    Instalando os pacotes do entityframework

---
+++ CentralDeUsuarios/CentralDeUsuarios.Domain/Interfaces/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,25 @@
+﻿using CentralDeUsuarios.Domain.Core;
+using CentralDeUsuarios.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeUsuarios.Domain.Interfaces.Repositories
+{
+    /// <summary>
+    /// Interface de repositório para Usuário
+    /// </summary>
+    public interface IUsuarioRepository : IBaseRepository<Usuario, Guid>
+        
+    {
+        /// <summary>
+        /// Método para consultar 1 usuário por email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        Usuario GetByEmail(string email);
+
+    }
+}
