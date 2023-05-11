da926f6e63ae4e554a14ece1c0755c0570cb1154
wandrade23 <wilson.felix.jr@gmail.com>
2023-05-11 00:21:30 -0300

    Exclusão da classe gerada automáticamente pelo VS e criação de uma nova controller UsuáriosController

---
+++ CentralDeUsuarios/CentralDeUsuarios.Services.Api/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralDeUsuarios.Services.Api.Controllers
+{
+    [Route("api/[controller]")] //Mapea a rota
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Post ()
+        {
+        
+            return Ok();
+        
+        }
+
+    }
+}
