c8aa051167378a2a6f308802678cfb861d57af13
wandrade23 <wilson.felix.jr@gmail.com>
2023-05-11 05:47:46 -0300

    Executando migration e criando camada de teste TDD

---
+++ CentralDeUsuarios/CentralDeUsuarios.UnitTests/Repositories/UsuarioRepositoryTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CentralDeUsuarios.UnitTests.Repositories
+{
+    /// <summary>
+    /// Classe de teste para o repositório de usuário
+    /// </summary>
+    public class UsuarioRepositoryTest
+    {
+        [Fact(Skip ="Não implementedo.")]
+        public void TestCreate() 
+        { 
+        
+        
+        
+        }
+
+        [Fact(Skip = "Não implementedo.")]
+        public void TestUpdate()
+        {
+
+
+
+        }
+
+        [Fact(Skip = "Não implementedo.")]
+        public void TestDelete()
+        {
+
+
+
+        }
+
+        [Fact(Skip = "Não implementedo.")]
+        public void TestGetAll()
+        {
+
+
+
+        }
+
+        [Fact(Skip = "Não implementedo.")]
+        public void TestGetById()
+        {
+
+
+
+        }
+
+
+        [Fact(Skip = "Não implementedo.")]
+        public void TestGetByEmail()
+        {
+
+
+
+        }
+    }
+}
