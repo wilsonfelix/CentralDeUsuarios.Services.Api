f5b3fbe9c53ddb7088ff35c062f27eb46b0659fe
wandrade23 <wilson.felix.jr@gmail.com>
2023-05-11 05:02:59 -0300

    Crindo a classe para conexão com o banco de dados (Temporáriamente)

---
+++ CentralDeUsuarios/CentralDeUsuarios.Infra.Data/Contexts/SqlServerContext.cs
@@ -0,0 +1,41 @@
+﻿using CentralDeUsuarios.Domain.Entities;
+using CentralDeUsuarios.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeUsuarios.Infra.Data.Contexts
+{
+    /// <summary>
+    /// Classe de contesto do banco de dados
+    /// </summary>
+    public class SqlServerContext : DbContext
+    {
+        /// <summary>
+        /// Método para definir a connectionstring do projeto
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.,1433;Initial Catalog=DB_CentralDeUsuários;Persist Security Info=True;User ID=sa;Password=P@ssw0rd;Pooling=False");
+        }
+
+        /// <summary>
+        /// Método para adicionar cada classe de mapeamento
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+        }
+        /// <summary>
+        /// Propriedade para fornecer os métodos que
+        /// serão utilizados no repositório de usuários
+        /// </summary>
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
---
+++ CentralDeUsuarios/CentralDeUsuarios.Infra.Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CentralDeUsuarios.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CentralDeUsuarios.Infra.Data.Mappings
+{
+    /// <summary>
+    /// Classe de mapeamento ORM da entidade usuário
+    /// </summary>
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+                
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasIndex(u => u.Email).IsUnique();
+            //builder.HasIndex(u => u.Email2).IsUnique();
+        }
+    }
+}
